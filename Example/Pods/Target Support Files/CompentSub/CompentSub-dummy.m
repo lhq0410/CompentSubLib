b4b22eb7df9cf5611d9ef57bc84a0a8984aa736b
李海强 <>
2019-12-15 10:06:01 +0800

    初始化

---
+++ Example/Pods/Target Support Files/CompentSub/CompentSub-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CompentSub : NSObject
+@end
+@implementation PodsDummy_CompentSub
+@end
---
+++ Example/Pods/Target Support Files/Pods-CompentSub_Example/Pods-CompentSub_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CompentSub_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CompentSub_Example
+@end
